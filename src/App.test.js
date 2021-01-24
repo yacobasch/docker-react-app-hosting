53623365a979f3b72b04834c1930c3dfe8bac222
Yacob T <ya.tsegaye@gmail.com>
2021-01-23 22:31:36 -0600

    Initial commit

---
+++ src/App.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+//Another test method call
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
